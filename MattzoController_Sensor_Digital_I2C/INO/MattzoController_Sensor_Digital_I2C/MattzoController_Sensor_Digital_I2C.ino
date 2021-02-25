cbeb2f8460261b4e3472ac7ca9062a65c4b73a88
Back Kevin <back_kevin@hotmail.com>
2021-02-25 21:42:26 +0100

    Update MattzoController_Sensor_Digital_I2C.ino

--- MattzoController_Sensor_Digital_I2C/INO/MattzoController_Sensor_Digital_I2C/MattzoController_Sensor_Digital_I2C.ino
+++ MattzoController_Sensor_Digital_I2C/INO/MattzoController_Sensor_Digital_I2C/MattzoController_Sensor_Digital_I2C.ino
@@ -19,12 +19,12 @@
 #include <PCF8574.h>                              //
 PCF8574 PCF_01(0x38);                             // set adr from PCF8574   ==> 000
 PCF8574 PCF_02(0x39);                             // set adr from PCF8574   ==> 100
-PCF8574 PCF_03(0x40);                             // set adr from PCF8574   ==> 010
-PCF8574 PCF_04(0x41);                             // set adr from PCF8574   ==> 110
-PCF8574 PCF_05(0x42);                             // set adr from PCF8574   ==> 001
-PCF8574 PCF_06(0x43);                             // set adr from PCF8574   ==> 101
-PCF8574 PCF_07(0x44);                             // set adr from PCF8574   ==> 011
-PCF8574 PCF_08(0x45);                             // set adr from PCF8574   ==> 111
+PCF8574 PCF_03(0x3A);                             // set adr from PCF8574   ==> 010
+PCF8574 PCF_04(0x3B);                             // set adr from PCF8574   ==> 110
+PCF8574 PCF_05(0x3C);                             // set adr from PCF8574   ==> 001
+PCF8574 PCF_06(0x3D);                             // set adr from PCF8574   ==> 101
+PCF8574 PCF_07(0x3E);                             // set adr from PCF8574   ==> 011
+PCF8574 PCF_08(0x3F);                             // set adr from PCF8574   ==> 111
 
 
 // Time in milliseconds until release event is reported after sensor has lost contact
@@ -52,140 +52,130 @@ void setup() {
   setupMattzoController();
 
   delay(500);
+
+  int countinit = 0;
   
   PCF_01.begin();
   if (!PCF_01.begin())
   {
     mcLog("1 could not initialize...");
   }else{
-    mcLog("=>1 PCF8574 is initialized");  
+    mcLog("=>PCF8574 NR.1 is initialized");  
   }
   if (!PCF_01.isConnected())
   {
     mcLog("=>(1) not connected");
     while(1);
   }else{
-    mcLog("=>1 PCF8574 is connected");  
+    countinit = countinit+1;
   }
 
-  if(NUM_SENSORS > 8 && NUM_SENSORS < 16)
+  if(NUM_SENSORS > 8 && NUM_SENSORS <= 16)
   {
     PCF_02.begin();
     if (!PCF_02.begin())
     {
       mcLog("(2) could not initialize...");
     }else{
-      mcLog("=>2 PCF8574 are initialized");  
+      mcLog("=>PCF8574 NR.2 is initialized");  
     }
     if (!PCF_02.isConnected())
     {
       mcLog("=>(2) not connected");
       while(1);
     }else{
-      mcLog("=>2 PCF8574 are connected");  
+      countinit = countinit+1;
     }
-  }else{
-    mcLog("=>only 1 PCF8574 is connected");
   }
 
-  if(NUM_SENSORS > 16 && NUM_SENSORS < 24)
+  if(NUM_SENSORS > 16 && NUM_SENSORS <= 24)
   {
     PCF_03.begin();
     if (!PCF_03.begin())
     {
       mcLog("(3) could not initialize...");
     }else{
-      mcLog("=>3 PCF8574 are initialized");  
+      mcLog("=>PCF8574 NR.3 is initialized");  
     }
     if (!PCF_03.isConnected())
     {
       mcLog("=>(3) not connected");
       while(1);
     }else{
-      mcLog("=>3 PCF8574 are connected");  
+      countinit = countinit+1;
     }
-  }else{
-    mcLog("=>only 2 PCF8574 is connected");
   }
 
-  if(NUM_SENSORS > 24 && NUM_SENSORS < 32)
+  if(NUM_SENSORS > 24 && NUM_SENSORS <= 32)
   {
     PCF_04.begin();
     if (!PCF_04.begin())
     {
       mcLog("(4) could not initialize...");
     }else{
-      mcLog("=>4 PCF8574 are initialized");  
+      //mcLog("=>PCF8574 NR.4 is initialized");  
     }
     if (!PCF_04.isConnected())
     {
       mcLog("=>(4) not connected");
       while(1);
-    }else{
-      mcLog("=>4 PCF8574 are connected");  
+    }else{  
+      countinit = countinit+1;
     }
-  }else{
-    mcLog("=>only 3 PCF8574 is connected");
   }
 
-  if(NUM_SENSORS > 32 && NUM_SENSORS < 40)
+  if(NUM_SENSORS > 32 && NUM_SENSORS <= 40)
   {
     PCF_05.begin();
     if (!PCF_05.begin())
     {
       mcLog("(5) could not initialize...");
     }else{
-      mcLog("=>5 PCF8574 are initialized");  
+      mcLog("=>PCF8574 NR.5 is initialized");  
     }
     if (!PCF_05.isConnected())
     {
       mcLog("=>(5) not connected");
       while(1);
     }else{
-      mcLog("=>5 PCF8574 are connected");  
+      countinit = countinit+1;
     }
-  }else{
-    mcLog("=>only 4 PCF8574 is connected");
   }
 
-  if(NUM_SENSORS > 40 && NUM_SENSORS < 48)
+  if(NUM_SENSORS > 40 && NUM_SENSORS <= 48)
   {
     PCF_06.begin();
     if (!PCF_06.begin())
     {
       mcLog("(6) could not initialize...");
     }else{
-      mcLog("=>6 PCF8574 are initialized");  
+      mcLog("=>PCF8574 NR.6 is initialized");  
     }
     if (!PCF_06.isConnected())
     {
       mcLog("=>(6) not connected");
       while(1);
-    }else{
-      mcLog("=>6 PCF8574 are connected");  
+    }else{ 
+      countinit = countinit+1;
     }
-  }else{
-    mcLog("=>only 5 PCF8574 is connected");
   }
 
-  if(NUM_SENSORS > 48 && NUM_SENSORS < 56)
+  if(NUM_SENSORS > 48 && NUM_SENSORS <= 56)
   {
     PCF_07.begin();
     if (!PCF_07.begin())
     {
       mcLog("(7) could not initialize...");
     }else{
-      mcLog("=>7 PCF8574 are initialized");  
+      mcLog("=>PCF8574 NR.7 is initialized");  
     }
     if (!PCF_07.isConnected())
     {
       mcLog("=>(7) not connected");
       while(1);
-    }else{
-      mcLog("=>7 PCF8574 are connected");  
+    }else{ 
+      countinit = countinit+1;
     }
-  }else{
-    mcLog("=>only 6 PCF8574 is connected");
   }
 
   if(NUM_SENSORS > 56 )
@@ -195,20 +185,22 @@ void setup() {
     {
       mcLog("(8) could not initialize...");
     }else{
-      mcLog("=>8 PCF8574 are initialized");  
+      mcLog("=>PCF8574 NR.8 is initialized");  
     }
     if (!PCF_08.isConnected())
     {
       mcLog("=>(8) not connected");
       while(1);
     }else{
-      mcLog("=>8 PCF8574 are connected");  
+      countinit = countinit+1;
     }
-  }else{
-    mcLog("=>only 7 PCF8574 is connected");
   }
 
+mcLog("=>Total "); 
+mcLog(String(countinit)); 
+mcLog(" PCF8574 are connected"); 
 
+  
   
 }
 
@@ -250,33 +242,35 @@ void monitorSensors() {
   for (int i = 0; i < NUM_SENSORS; i++) {
     //int sensorValue = digitalRead(SENSOR_PIN[i]);
     //int sensorValue = PCF_01.read(IN[i]);
-    
+
+    int b;
     int sensorValue;
+    
     if(i <= 7)
     {
-      i = i - 0;
-      sensorValue = PCF_01.read(IN[i]);
+      b = i;
+      sensorValue = PCF_01.read(IN[b]);
     }else if(i >= 8 && i <= 15){
-      i = i - 8;
-      sensorValue = PCF_02.read(IN[i]);  
+      b = i - 8;
+      sensorValue = PCF_02.read(IN[b]);  
     }else if(i >= 16 && i <= 23){
-      i = i - 16;
-      sensorValue = PCF_03.read(IN[i]);  
+      b = i - 16;
+      sensorValue = PCF_03.read(IN[b]);  
     }else if(i >= 24 && i <= 31){
-      i = i - 24;
-      sensorValue = PCF_04.read(IN[i]);  
+      b = i - 24;
+      sensorValue = PCF_04.read(IN[b]);  
     }else if(i >= 32 && i <= 39){
-      i = i - 32;
-      sensorValue = PCF_05.read(IN[i]);  
+      b = i - 32;
+      sensorValue = PCF_05.read(IN[b]);  
     }else if(i >= 40 && i <= 47){
-      i = i - 40;
-      sensorValue = PCF_06.read(IN[i]);  
+      b = i - 40;
+      sensorValue = PCF_06.read(IN[b]);  
     }else if(i >= 48 && i <= 55){
-      i = i - 48;
-      sensorValue = PCF_07.read(IN[i]);  
+      b = i - 48;
+      sensorValue = PCF_07.read(IN[b]);  
     }else if(i >= 56){
-      i = i - 56;
-      sensorValue = PCF_08.read(IN[i]);  
+      b = i - 56;
+      sensorValue = PCF_08.read(IN[b]);  
     }
 
     if (sensorValue == sensorTriggerState[i]) {
