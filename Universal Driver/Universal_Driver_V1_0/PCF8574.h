4ad005d5e2fcde89807c4ce2aa2078db73ff2831
Back Kevin <back_kevin@hotmail.com>
2021-04-06 00:03:10 +0200

    Update PCF8574.h

--- Universal Driver/Universal_Driver_V1_0/PCF8574.h
+++ Universal Driver/Universal_Driver_V1_0/PCF8574.h
@@ -19,7 +19,7 @@
 // ***************************
 
 // PCF8574 port expander used?
-#define USE_PCF8574 false
+#define USE_PCF8574 true
 
 // Number of chained PCF8574 port extenders
 #define NUM_PCF8574s 2
@@ -48,3 +48,8 @@ uint8_t PCF8574_ADR[8]= {0x20, 0x21 , 0x22, 0x23, 0x24, 0x25, 0x26, 0x27}; //don
 #if !TYPE_PCF8574
 uint8_t PCF8574_ADR[8]= {0x38, 0x39 , 0x38, 0x3A, 0x3B, 0x3C, 0x3E, 0x3F}; //don`t edit
 #endif
+
+// Number of signal ports for crossing
+const int NUM_CROSSINGPORTS = 8;
+uint8_t OUT2[16] = {LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW};// led state
+uint8_t OUT3[16] = {LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW};// blink state
