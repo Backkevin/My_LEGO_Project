b6fc97ac8040a58cb2d6f1ab377d17bfd0184ab4
Back Kevin <back_kevin@hotmail.com>
2021-04-05 02:30:12 +0200

    Create MCP23017.h

---
+++ Universal Driver/Universal_Driver_V1_0/MCP23017.h
@@ -0,0 +1,28 @@
+/*
+* UNIVERSAL DRIVER 
+* 
+* Author: BACK KEVIN
+* Copyright 2021 by Back Kevin
+* License:
+* Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+* 
+*/
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+// ***************************
+// Controller wiring specifics
+// ***************************
+
+// MCP23017 port expander used?
+#define USE_MCP23017 false
+
+// Number of chained PCA9685 port extenders
+#define NUM_MCP23017s 1
+
+// Addresses of MCP23017 port extenders
+uint8_t MCP23017_ADR[8]= {0x20, 0x21 , 0x22, 0x23, 0x24, 0x25, 0x26, 0x27}; //don`t edit
