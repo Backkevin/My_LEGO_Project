31d346cd03172c267f74de533a133ef5f0830729
Back Kevin <back_kevin@hotmail.com>
2021-04-15 14:15:15 +0200

    Update Config.h

--- Universal Driver/ONLY SIGNAL/Signal_V_1_0/Config.h
+++ Universal Driver/ONLY SIGNAL/Signal_V_1_0/Config.h
@@ -60,7 +60,7 @@ uint8_t STATUS_LED_PIN = D8; //D0 or D8 on CPU Board
 // ***************
 
 // Syslog application name
-const char* SYSLOG_APP_NAME = "OUT";
+const char* SYSLOG_APP_NAME = "OOUT";
 
 
 // ***************************
