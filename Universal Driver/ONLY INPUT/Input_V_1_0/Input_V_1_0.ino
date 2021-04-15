797a8e38f46267c0ba5b547499aa8949df604dac
Backkevin <back_kevin@hotmail.com>
2021-04-15 13:11:42 +0200

    Update Input_V_1_0.ino

--- Universal Driver/ONLY INPUT/Input_V_1_0/Input_V_1_0.ino
+++ Universal Driver/ONLY INPUT/Input_V_1_0/Input_V_1_0.ino
@@ -1,9 +1,206 @@
+/*
+ * PCF8574 written but not tested
+ */
+
+#define MATTZO_CONTROLLER_TYPE "IN"
+
+
+#include <ESP8266WiFi.h>                          // WiFi library for ESP-8266
+#include <Servo.h>                                // Servo library
+#include <Wire.h>                                 // Built-in library for I2C
+#include "Config.h"                               // this file should be placed in the same folder
+#include "MattzoController_Library.h"             // this file needs to be placed in the Arduino library folder
+
+// ******************
+// PCF8574 settings
+// ******************
+
+#if USE_PCF8574
+#include <PCF8574.h> 
+PCF8574 PCF_01(PCF8574_ADR[0]);                             // set adr from PCF8574   ==> 000 
+PCF8574 PCF_02(PCF8574_ADR[1]);                             // set adr from PCF8574   ==> 100 
+PCF8574 PCF_03(PCF8574_ADR[2]);                             // set adr from PCF8574   ==> 010 
+PCF8574 PCF_04(PCF8574_ADR[3]);                             // set adr from PCF8574   ==> 110 
+PCF8574 PCF_05(PCF8574_ADR[4]);                             // set adr from PCF8574   ==> 001 
+PCF8574 PCF_06(PCF8574_ADR[5]);                             // set adr from PCF8574   ==> 101 
+PCF8574 PCF_07(PCF8574_ADR[6]);                             // set adr from PCF8574   ==> 011 
+PCF8574 PCF_08(PCF8574_ADR[7]);                             // set adr from PCF8574   ==> 111               
+#endif
+
+// ******************
+// MCP23017 settings
+// ******************
+
+#if USE_MCP23017
+#include "Adafruit_MCP23017.h"
+Adafruit_MCP23017 mcp23017[NUM_MCP23017s];
+#endif
+
+
+// ******************
+// INPUT settings
+// ******************
+
+// Time in milliseconds until release event is reported after sensor has lost contact
+const int SENSOR_RELEASE_TICKS = 100;
+bool sensorState[NUM_SENSORS];
+int sensorTriggerState[NUM_SENSORS];
+int lastSensorContactMillis[NUM_SENSORS];
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////START SETUP
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void setup() {
-  // put your setup code here, to run once:
+  
+Wire.begin();
+  
+delay(500);
+  
+// load config from EEPROM, initialize Wifi, MQTT etc.
+setupMattzoController();
+
+delay(500);
+
+mcLog(" Configured as INPUT"); 
+
+#if USE_PCA9685
+  mcLog(" USE PCF8574"); 
+  setupPCA9685();
+#endif
+
+#if USE_MCP23017
+  mcLog(" USE MCP23017");
+  setupMCP23017();
+#endif
 
 }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////END SETUP
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+
+#if USE_PCF8574
+void setupPCF8574() {
+  if(NUM_PCF8574s >= 1){PCF_01.begin();}                        
+  if(NUM_PCF8574s >= 2){PCF_02.begin();}                             
+  if(NUM_PCF8574s >= 3){PCF_03.begin();}                              
+  if(NUM_PCF8574s >= 4){PCF_04.begin();}                             
+  if(NUM_PCF8574s >= 5){PCF_05.begin();}                             
+  if(NUM_PCF8574s >= 6){PCF_06.begin();}                             
+  if(NUM_PCF8574s >= 7){PCF_07.begin();}                            
+  if(NUM_PCF8574s >= 8){PCF_08.begin();}      
+}
+#endif
+
+
+#if USE_MCP23017
+void setupMCP23017() {
+  for (int m = 0; m < NUM_MCP23017s; m++) {
+    mcp23017[m] = Adafruit_MCP23017(MCP23017_ADR[m]);
+    mcp23017[m].begin();
+  }
+}
+#endif
+
+
+void mqttCallback(char* topic, byte* payload, unsigned int length) {
+  // MattzoSensorControllers do not care about received mqtt messages.
+  // => No operation required.
+}
+
+void sendSensorEvent2MQTT(int sensorPort, int sensorState) {
+  String sensorRocId = MATTZO_CONTROLLER_TYPE + String(mattzoControllerId) + "-" + String(sensorPort + 1);  // e.g. "MattzoController12345-3"
+  String stateString = sensorState ? "true" : "false";
+
+  // compile mqtt message. Parameters:
+  //   id: Combination of sensor name and port (e.g. MattzoController12345-3). The reported port (the "logic" port) is 1 count higher than the internal port number in the sensor, e.g. port 2 in the sensor equals 3 in Rocrail)
+  //   bus: controller number
+  //   address: port number (internal port number plus 1)
+  // both id or bus/address can be used in Rocrail. If id is used, it superseeds the combination of bus and address
+  String mqttMessage = "<fb id=\"" + sensorRocId + "\" bus=\"" + String(mattzoControllerId) + "\" addr=\"" + String(sensorPort + 1) + "\" state=\"" + stateString + "\"/>";
+  mcLog("Sending MQTT message: " + mqttMessage);
+  char mqttMessage_char[255];   // message is usually 61 chars, so 255 chars should be enough
+  mqttMessage.toCharArray(mqttMessage_char, mqttMessage.length() + 1);
+  mqttClient.publish("rocrail/service/client", mqttMessage_char);
+}
+
+// Switches LED on if one or more sensors has contact
+// Switches LED off if no sensor has contact
+void setLEDBySensorStates() {
+  bool ledOnOff = false;
+  for (int i = 0; i < NUM_SENSORS; i++) {
+    if (sensorState[i]) {
+      statusLEDState = true;
+      return;
+    }
+  }
+  statusLEDState = false;
+}
+
+void monitorSensors() {
+  for (int i = 0; i < NUM_SENSORS; i++) {
+    int b;
+    int sensorValue;
+    
+    if(i <= 7)
+    {
+      b = i;
+      sensorValue = PCF_01.read(IN[b]);
+    }else if(i >= 8 && i <= 15){
+      b = i - 8;
+      sensorValue = PCF_02.read(IN[b]);  
+    }else if(i >= 16 && i <= 23){
+      b = i - 16;
+      sensorValue = PCF_03.read(IN[b]);  
+    }else if(i >= 24 && i <= 31){
+      b = i - 24;
+      sensorValue = PCF_04.read(IN[b]);  
+    }else if(i >= 32 && i <= 39){
+      b = i - 32;
+      sensorValue = PCF_05.read(IN[b]);  
+    }else if(i >= 40 && i <= 47){
+      b = i - 40;
+      sensorValue = PCF_06.read(IN[b]);  
+    }else if(i >= 48 && i <= 55){
+      b = i - 48;
+      sensorValue = PCF_07.read(IN[b]);  
+    }else if(i >= 56){
+      b = i - 56;
+      sensorValue = PCF_08.read(IN[b]);  
+    }
+
+    if (sensorValue == sensorTriggerState[i]) {
+      // Contact -> report contact immediately
+      if (!sensorState[i]) {
+        mcLog("Sensor " + String(i) + " triggered.");
+        sendSensorEvent2MQTT(i, true);
+        sensorState[i] = true;
+      }
+      lastSensorContactMillis[i] = millis();
+    } else {
+      // No contact for SENSOR_RELEASE_TICKS milliseconds -> report sensor has lost contact
+      if (sensorState[i] && (millis() > lastSensorContactMillis[i] + SENSOR_RELEASE_TICKS)) {
+        mcLog("Sensor " + String(i) + " released.");
+        sendSensorEvent2MQTT(i, false);
+        sensorState[i] = false;
+      }
+    }
+  }
+
+  setLEDBySensorStates();
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////START LOOP
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  loopMattzoController();
+  monitorSensors();
 
 }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////END LOOP
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
