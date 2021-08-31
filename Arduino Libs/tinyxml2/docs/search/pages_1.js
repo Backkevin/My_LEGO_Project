cbea35127757c6b5d24d90e18897e564ed59a794
Backkevin <back_kevin@hotmail.com>
2021-08-31 22:16:34 +0200

    Delete Arduino Libs directory

--- Arduino Libs/tinyxml2/docs/search/pages_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['load_20an_20xml_20file_281',['Load an XML File',['../_example_1.html',1,'']]]
-];
--- Arduino Libs/tinyxml2/docs/search/pages_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['tinyxml_2d2_284',['TinyXML-2',['../index.html',1,'']]]
-];
